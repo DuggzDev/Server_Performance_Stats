fe5095e319183799bef2894baf6758f83269ff15
duggz <duggz@James.>
2025-03-18 01:30:44 +0000

    server stats script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # DevOpz
 Learning Purposes
+
+To run the script:
+
+source script.sh
+
+https://roadmap.sh/projects/server-stats
---
+++ script.sh
@@ -0,0 +1,18 @@
+# Total CPU Usage
+top -bn1 | grep "Cpu(s)" | awk '{print "Total CPU Usage: " 100 - $8 "%"}'
+
+#Total Memory Usage, free & used as a percentage
+free -m | grep "Mem:" | awk '{print "Total Free memory: " $4/100 "%"}'
+free -m | grep "Mem:" | awk '{print "Total Used memory: " $3/100 "%"}'
+
+#Total Disk Usage, free & used as a percentage
+df -h --total | grep 'total' | awk '{print "Total Disk Usage: " $5}'
+df -h --total | grep 'total' | awk '{print "Total Disk space free: " 100 - $5 "%"}'
+
+#Top 5 process by CPU Usage
+printf "\nTop 5 process by CPU Usage:\n"
+ps -eo pid,comm,%cpu --sort=-%cpu | head -6 
+
+#Top 5 process by CPU Usage
+printf "\nTop 5 process by CPU Memory Usage:\n"
+ps -eo pid,comm,%mem --sort=-%mem | head -6 
